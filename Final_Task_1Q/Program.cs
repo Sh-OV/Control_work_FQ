5fe76183cb63df5511316ec9af4b1207d8b52e3d
Sh-OV <shchegolga@yandex.ru>
2022-08-07 13:04:31 +0500

    Добавила метод печати строкового одномерного массива

--- Final_Task_1Q/Program.cs
+++ Final_Task_1Q/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Условия итоговой задачи за 1 четверть читай в файле README.md
-Console.Clear();
-string[] UserArray()
+//Console.Clear();
+string[] UserArray()                    // метод получения строкового массива от пользователя
 {
     Console.Write("Введите количество элементов строкового массива: ");
     int count = Convert.ToInt32(Console.ReadLine());
@@ -8,16 +8,16 @@ string[] UserArray()
     Console.Write($"Введите {count} элементов текста (через Enter): ");
     for (int i = 0; i < userarray.Length; i++)
     {
-        if (Console.ReadLine() is not null)
-        {
             userarray[i] = Console.ReadLine();
-        }
     }
     return userarray;
 }
 string[] uarray = UserArray();
+Console.WriteLine();
+PrintArr(uarray);
+Console.Write("  ->  ");
 
-string[] SelectArray(string[] array)
+string[] SelectArray(string[] array)        // метод выбора элементов входного массива по условию (путем создания нового массива)
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
@@ -34,6 +34,19 @@ string[] SelectArray(string[] array)
             k++;
         }
     }
+    if (count == 0) Console.WriteLine("[]");
     return selectarray;
 }
 string[] sarray = SelectArray(uarray);
+
+void PrintArr (string[] res)  // метод вывода строкового одномерного массива на печать
+{
+    for (int i = 0; i < res.Length; i++)
+    {
+        if (i == 0) Console.Write($"[");
+        if ( i < res.Length-1) Console.Write($"{res[i]}, ");
+        else Console.Write($"{res[i]}]");
+    }
+}
+PrintArr(sarray);
+Console.WriteLine();
